522a4cb86abdde2eeadb0eada1d36d3fb644845f
icosagon8 <dmpavlov8@yandex.ru>
2022-03-09 22:05:40 +0400

    feat: create feature modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseModule } from './features/course/course.module';
+import { CoursesModule } from './features/courses/courses.module';
+import { LoginModule } from './features/login/login.module';
+import { RegistrationModule } from './features/registration/registration.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        LoginModule,
+        RegistrationModule,
+        CoursesModule,
+        CourseModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
