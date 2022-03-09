176e5e4b9bf6592ec87864362bc7994c441b6f58
icosagon8 <dmpavlov8@yandex.ru>
2022-03-09 22:34:44 +0400

    feat: add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+const COMPONENTS = [];
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+    exports: [CommonModule],
+})
+export class SharedModule {}
